609c1b1917b3491b4a4a997cc3eb497b902e89de
Fred Snyder <fsnyder@castwide.com>
2017-02-14 14:04:21 -0500

    First commit.

---
+++ src/extension.ts
@@ -0,0 +1,95 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+let child_process = require('child_process');
+let request = require('request');
+
+let solargraphProcess = null;
+
+const completionProvider = {
+    provideCompletionItems: function completionProvider(document, position) {
+        return new Promise((resolve, reject) => {
+            const kinds = {
+                "Class": vscode.CompletionItemKind.Class,
+                "Keyword": vscode.CompletionItemKind.Keyword,
+                "Module": vscode.CompletionItemKind.Module,
+                "Method": vscode.CompletionItemKind.Method,
+                "Variable": vscode.CompletionItemKind.Variable,
+                "Snippet": vscode.CompletionItemKind.Snippet
+            }
+            console.log('Posting');
+            request.post({ url: 'http://localhost:56527/suggest', form: { script: document.getText(), index: document.offsetAt(position) }}, function(error, response, body) {
+                // HACK: Tricking the type system to avoid an invalid error
+                var SnippetString = vscode['SnippetString'];
+                if (!error && response.statusCode == 200) {
+                    if (body == "") {
+                        return resolve([]);
+                    } else {
+                        console.log(body);
+                        let result = JSON.parse(body);
+                        let items = [];
+                        if (result.status == "ok") {
+                            var range = document.getWordRangeAtPosition(position);
+                            if (range) {
+                                var repl = document.getText(range);
+                                if (range.start.character > 0) {
+                                    if (repl.substr(0, 1) == ':') {
+                                        var prevChar = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.start.line, range.start.character));
+                                        if (prevChar == ':') {
+                                            // Replacement range starts with a colon, but there's
+                                            // a previous colon. That means we're in a namespace,
+                                            // not a symbol. Get rid of the colon in the namespace
+                                            // range.
+                                            range = new vscode.Range(range.start.line, range.start.character + 1, range.end.line, range.end.character);
+                                        }
+                                    }
+                                }
+                            }
+                            result.suggestions.forEach((cd) => {
+                                var item = new vscode.CompletionItem(cd['label'], kinds[cd['kind']]);
+                                // Treat instance variables slightly differently
+                                if (cd['insert'].substring(0, 1) == '@') {
+                                    item.insertText = cd['insert'].substring(1);
+                                } else {
+                                    item.insertText = new SnippetString(cd['insert']);
+                                }
+                                if (range) {
+                                    item['range'] = range;
+                                }
+                                item.detail = cd['kind'];
+                                item.documentation = cd['detail'];
+                                items.push(item);
+                            });
+                            return resolve(items);
+                        } else {
+                            return resolve([]);
+                        }
+                    }
+                }
+            });
+        });
+    }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            'ruby', completionProvider, "."
+        )
+    );
+    solargraphProcess = child_process.spawn('ruby', [context.extensionPath + '/src/server.rb']);
+    solargraphProcess.stderr.on('data', (data) => {
+        console.log('[stderr from Solargraph server] ' + data);
+    });
+    console.log('Solargraph extension activated.');
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    console.log('Deactivating extension');
+    solargraphProcess.kill();
+}
---
+++ src/server.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift "s:/solargraph/lib"
+require 'solargraph'
+require 'json'
+require 'sinatra'
+
+set :port, 56527
+
+api_map = Solargraph::ApiMap.new
+api_map.merge(Parser::CurrentRuby.parse(File.read("#{Solargraph::STUB_PATH}/ruby/2.3.0/core.rb")))
+api_map.freeze
+
+post '/suggest' do
+  content_type :json
+  begin
+    map = Solargraph::CodeMap.new(params['script'], api_map: api_map)
+    sugg = map.suggest_at(params['index'].to_i)
+    result = { "status" => "ok", "suggestions" => sugg }
+    result.to_json
+  rescue Exception => e
+    STDERR.puts e
+    STDERR.puts e.backtrace.join("\n")
+    { "status" => "err", "message" => e.message }.to_json
+  end
+end
