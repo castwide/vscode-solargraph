00a9b1c0692b648df9436b45a1cca78feaaa577d
Fred Snyder <fsnyder@castwide.com>
2017-03-18 10:15:14 -0400

    Switched to command-based suggestions instead of server. Changed configuration options.

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,30 @@ import * as child_process from 'child_process';
 import * as request from 'request';
 import * as fs from 'fs';
 
-let solargraphProcess = null;
-let solargraphPort = null;
+function solargraphCommand(args) {
+	let cmd = [];
+	if (process.platform === 'win32') cmd.push('cmd', '/c');
+	if (vscode.workspace.getConfiguration('ruby').solargraph.useBundler) {
+		// TODO: pathToBundler configuration
+		cmd.push('bundle', 'exec');
+	}
+	cmd.push(vscode.workspace.getConfiguration('ruby').solargraph.commandPath);
+	return child_process.spawn(cmd.shift(), cmd.concat(args));
+}
+
+function yardCommand(args) {
+	let cmd = [];
+	if (process.platform === 'win32') cmd.push('cmd', '/c');
+	if (vscode.workspace.getConfiguration('ruby').solargraph.useBundler) {
+		// TODO: pathToBundler configuration
+		cmd.push('bundle', 'exec');
+	}
+	cmd.push('yard');
+	return child_process.spawn(cmd.shift(), cmd.concat(args));
+}
 
 const completionProvider = {
-    provideCompletionItems: function completionProvider(document: vscode.TextDocument, position: vscode.Position) {
+    provideCompletionItems: function completionProvider(document, position) {
         return new Promise((resolve, reject) => {
             const kinds = {
                 "Class": vscode.CompletionItemKind.Class,
@@ -19,109 +38,84 @@ const completionProvider = {
                 "Variable": vscode.CompletionItemKind.Variable,
                 "Snippet": vscode.CompletionItemKind.Snippet
             }
-            if (!solargraphPort) {
-                return resolve([]);
-            }
-            request.post({ url: 'http://localhost:' + solargraphPort + '/suggest', form: { filename: document.fileName, text: document.getText(), line: position.line, col: position.character }}, function(error, response, body) {
-                // HACK: Tricking the type system to avoid an invalid error
-                var SnippetString = vscode['SnippetString'];
-                if (!error && response.statusCode == 200) {
-                    if (body == "") {
-                        return resolve([]);
-                    } else {
-                        //console.log(body);
-                        let result = JSON.parse(body);
-                        let items = [];
-                        if (result.status == "ok") {
-                            var range = document.getWordRangeAtPosition(position);
-                            if (range) {
-                                var repl = document.getText(range);
-                                if (range.start.character > 0) {
-                                    if (repl.substr(0, 1) == ':') {
-                                        var prevChar = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.start.line, range.start.character));
-                                        if (prevChar == ':') {
-                                            // Replacement range starts with a colon, but there's
-                                            // a previous colon. That means we're in a namespace,
-                                            // not a symbol. Get rid of the colon in the namespace
-                                            // range.
-                                            range = new vscode.Range(range.start.line, range.start.character + 1, range.end.line, range.end.character);
-                                        }
-                                    }
-                                }
-                            }
-                            result.suggestions.forEach((cd) => {
-                                var item = new vscode.CompletionItem(cd['label'], kinds[cd['kind']]);
-                                // Treat instance variables slightly differently
-                                if (cd['insert'].substring(0, 1) == '@') {
-                                    item.insertText = cd['insert'].substring(1);
-                                } else {
-                                    item.insertText = new SnippetString(cd['insert']);
-                                }
-                                if (range) {
-                                    // HACK: Unrecognized property
-                                    item['range'] = range;
-                                }
-                                item.detail = cd['kind'];
-                                item.documentation = cd['documentation'];
-                                items.push(item);
-                            });
-                            return resolve(items);
-                        } else {
-                            return resolve([]);
-                        }
-                    }
-                }
-            });
+			// HACK: Tricking the type system to avoid an invalid error
+			let SnippetString = vscode['SnippetString'];
+			let child = solargraphCommand([
+				'suggest',
+				'--line=' + position.line,
+				'--column=' + position.character,
+				'--filename=' + document.fileName
+			]);
+			let errbuf = [], outbuf = [];
+			child.stderr.on('data', (data) => errbuf.push(data));
+			child.stdout.on('data', (data) => outbuf.push(data));
+			child.on('exit', () => {
+				var data = outbuf.join('');
+				if (data == "") {
+					return resolve([]);
+				} else {
+					let result = JSON.parse(data);
+					let items = [];
+					if (result.status == "ok") {
+						var range = document.getWordRangeAtPosition(position);
+						if (range) {
+							var repl = document.getText(range);
+							if (range.start.character > 0) {
+								if (repl.substr(0, 1) == ':') {
+									var prevChar = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.start.line, range.start.character));
+									if (prevChar == ':') {
+										// Replacement range starts with a colon, but there's
+										// a previous colon. That means we're in a namespace,
+										// not a symbol. Get rid of the colon in the namespace
+										// range.
+										range = new vscode.Range(range.start.line, range.start.character + 1, range.end.line, range.end.character);
+									}
+								}
+							}
+						}
+						result.suggestions.forEach((cd) => {
+							var item = new vscode.CompletionItem(cd['label'], kinds[cd['kind']]);
+							// Treat instance variables slightly differently
+							if (cd['insert'].substring(0, 1) == '@') {
+								item.insertText = cd['insert'].substring(1);
+							} else {
+								item.insertText = new SnippetString(cd['insert']);
+							}
+							if (range) {
+								// HACK: Unrecognized property
+								item['range'] = range;
+							}
+							item.detail = cd['kind'];
+							item.documentation = cd['documentation'];
+							items.push(item);
+						});
+						return resolve(items);
+					} else {
+						return resolve([]);
+					}
+				}
+			});
+			child.on('error', () => {
+				if (errbuf.length) {
+					console.log(errbuf.join("\n"));
+				}
+			});
+			child.stdin.end(document.getText());
         });
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider(
-            'ruby', completionProvider, "."
-        )
-    );
-
-    var isWin = /^win/.test(process.platform);
-
-    // Start the Solargraph server
-    var cmd = ['solargraph', 'server'];
-    var cwd = null;
-    if (vscode.workspace.rootPath) {
-        // TODO: Check for a gemfile
-        cmd.unshift('bundle', 'exec');
-        cwd = vscode.workspace.rootPath;
-    }
-    if (isWin) {
-        cmd.unshift('powershell');
-    }
-    solargraphProcess = child_process.spawn(cmd.shift(), cmd, { cwd: cwd });
-    solargraphProcess.stderr.on('data', (data) => {
-        console.log('[stderr from Solargraph server] ' + data);
-        var match = data.toString().match(/port=([0-9]*)/);
-        if (match) {
-            solargraphPort = match[1];
-        }
-    });
-    solargraphProcess.stdout.on('data', (data) => {
-        console.log('[stdout from Solargraph server] ' + data);
-    });
-
-    // Document gems
-    cmd = ['yard', 'gems'];
-    if (vscode.workspace.rootPath) {
-        cmd.unshift('bundle', 'exec');        
-    }
-    if (isWin) {
-        cmd.unshift('powershell');
-    }
-    child_process.spawn(cmd.shift(), cmd, { cwd: cwd });
+	const solargraphTest = solargraphCommand(['prepare']);
+	solargraphTest.on('exit', () => {
+		context.subscriptions.push(vscode.languages.registerCompletionItemProvider('ruby', completionProvider, '.'));
+		yardCommand(['gems']);
+	});
+	solargraphTest.on('error', () => 0);
 
     console.log('Solargraph extension activated.');
 }
 
 export function deactivate() {
     console.log('Deactivating extension');
-    solargraphProcess.kill();
 }
