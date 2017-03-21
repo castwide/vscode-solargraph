c419e56835a4abe982ba4a866370c82068cc288b
Fred Snyder <fsnyder@castwide.com>
2017-03-21 10:32:05 -0400

    Server option.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,5 +9,6 @@ All notable changes to the "solargraph" extension will be documented in this fil
 - Added icon
 - Reduced console output
 
-## 0.0.3
+## 0.1.0
 - Check for Solargraph gem updates
+- Use server instead of command line for better performance
--- src/extension.ts
+++ src/extension.ts
@@ -45,85 +45,111 @@ function gemCommand(args) {
 	return child_process.spawn(cmd.shift(), cmd.concat(args), env);
 }
 
+function getCompletionItems(data, document:vscode.TextDocument, position: vscode.Position):Array<vscode.CompletionItem> {
+	const kinds = {
+		"Class": vscode.CompletionItemKind.Class,
+		"Keyword": vscode.CompletionItemKind.Keyword,
+		"Module": vscode.CompletionItemKind.Module,
+		"Method": vscode.CompletionItemKind.Method,
+		"Variable": vscode.CompletionItemKind.Variable,
+		"Snippet": vscode.CompletionItemKind.Snippet
+	}
+	// HACK: Tricking the type system to avoid an invalid error
+	let SnippetString = vscode['SnippetString'];
+	let items:Array<vscode.CompletionItem> = [];
+	if (data.status == "ok") {
+		var range = document.getWordRangeAtPosition(position);
+		if (range) {
+			var repl = document.getText(range);
+			if (range.start.character > 0) {
+				if (repl.substr(0, 1) == ':') {
+					var prevChar = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.start.line, range.start.character));
+					if (prevChar == ':') {
+						// Replacement range starts with a colon, but there's
+						// a previous colon. That means we're in a namespace,
+						// not a symbol. Get rid of the colon in the namespace
+						// range.
+						range = new vscode.Range(range.start.line, range.start.character + 1, range.end.line, range.end.character);
+					}
+				}
+			}
+		}
+		data.suggestions.forEach((cd) => {
+			var item = new vscode.CompletionItem(cd['label'], kinds[cd['kind']]);
+			// Treat instance variables slightly differently
+			if (cd['insert'].substring(0, 1) == '@') {
+				item.insertText = cd['insert'].substring(1);
+			} else {
+				item.insertText = new SnippetString(cd['insert']);
+			}
+			if (range) {
+				// HACK: Unrecognized property
+				item['range'] = range;
+			}
+			item.detail = cd['detail'];
+			item.documentation = cd['documentation'];
+			items.push(item);
+		});
+	}
+	return items;
+}
+
+let solargraphServer:child_process.ChildProcess = null;
+let solargraphPort:String = null;
+
 const completionProvider = {
     provideCompletionItems: function completionProvider(document: vscode.TextDocument, position: vscode.Position) {
         return new Promise((resolve, reject) => {
-			console.log('Running the completion provider line ' + position.line + ', column ' + position.character);
-			console.log('Workspace is ' + vscode.workspace.rootPath);
-            const kinds = {
-                "Class": vscode.CompletionItemKind.Class,
-                "Keyword": vscode.CompletionItemKind.Keyword,
-                "Module": vscode.CompletionItemKind.Module,
-                "Method": vscode.CompletionItemKind.Method,
-                "Variable": vscode.CompletionItemKind.Variable,
-                "Snippet": vscode.CompletionItemKind.Snippet
-            }
-			// HACK: Tricking the type system to avoid an invalid error
-			let SnippetString = vscode['SnippetString'];
-			let child = solargraphCommand([
-				'suggest',
-				'--line=' + position.line,
-				'--column=' + position.character,
-				'--filename=' + document.fileName
-			]);
-			let errbuf = [], outbuf = [];
-			child.stderr.on('data', (data) => {
-				console.log(data.toString());
-				errbuf.push(data);
-			});
-			child.stdout.on('data', (data) => outbuf.push(data));
-			child.on('exit', () => {
-				var data = outbuf.join('');
-				if (data == "") {
-					return resolve([]);
-				} else {
-					let result = JSON.parse(data);
-					let items = [];
-					if (result.status == "ok") {
-						var range = document.getWordRangeAtPosition(position);
-						if (range) {
-							var repl = document.getText(range);
-							if (range.start.character > 0) {
-								if (repl.substr(0, 1) == ':') {
-									var prevChar = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.start.line, range.start.character));
-									if (prevChar == ':') {
-										// Replacement range starts with a colon, but there's
-										// a previous colon. That means we're in a namespace,
-										// not a symbol. Get rid of the colon in the namespace
-										// range.
-										range = new vscode.Range(range.start.line, range.start.character + 1, range.end.line, range.end.character);
-									}
-								}
-							}
-						}
-						result.suggestions.forEach((cd) => {
-							var item = new vscode.CompletionItem(cd['label'], kinds[cd['kind']]);
-							// Treat instance variables slightly differently
-							if (cd['insert'].substring(0, 1) == '@') {
-								item.insertText = cd['insert'].substring(1);
-							} else {
-								item.insertText = new SnippetString(cd['insert']);
-							}
-							if (range) {
-								// HACK: Unrecognized property
-								item['range'] = range;
-							}
-							item.detail = cd['detail'];
-							item.documentation = cd['documentation'];
-							items.push(item);
-						});
-						return resolve(items);
+			console.log(solargraphServer + ', ' + solargraphPort);
+			if (solargraphServer && solargraphPort) {
+				request.post({url:'http://localhost:' + solargraphPort + '/suggest', form: {
+					text: document.getText(),
+					filename: document.fileName,
+					line: position.line,
+					column: position.character}
+				}, function(err,httpResponse,body) {
+					if (err) {
+						console.log(err);
 					} else {
-						return resolve([]);
+						if (httpResponse.statusCode == 200) {
+							console.log(body);
+							return resolve(getCompletionItems(JSON.parse(body), document, position));
+						} else {
+							// TODO: Handle error
+						}
 					}
-				}
-			});
-			child.on('error', () => {
-				if (errbuf.length) {
-					console.log(errbuf.join("\n"));
-				}
-			});
-			child.stdin.end(document.getText());
+				});
+			} else {
+				console.log('Running the completion provider line ' + position.line + ', column ' + position.character);
+				console.log('Workspace is ' + vscode.workspace.rootPath);
+				let child = solargraphCommand([
+					'suggest',
+					'--line=' + position.line,
+					'--column=' + position.character,
+					'--filename=' + document.fileName
+				]);
+				let errbuf = [], outbuf = [];
+				child.stderr.on('data', (data) => {
+					console.log(data.toString());
+					errbuf.push(data);
+				});
+				child.stdout.on('data', (data) => outbuf.push(data));
+				child.on('exit', () => {
+					var data = outbuf.join('');
+					if (data == "") {
+						return resolve([]);
+					} else {
+						let result = JSON.parse(data);
+						return resolve(getCompletionItems(result, document, position));
+					}
+				});
+				child.on('error', () => {
+					if (errbuf.length) {
+						console.log(errbuf.join("\n"));
+					}
+				});
+				child.stdin.end(document.getText());
+			}
         });
     }
 }
@@ -153,10 +179,21 @@ export function activate(context: vscode.ExtensionContext) {
 		checkGemVersion();
 		context.subscriptions.push(vscode.languages.registerCompletionItemProvider('ruby', completionProvider, '.'));
 		yardCommand(['gems']);
-		if (vscode.workspace.rootPath) {
-			yardCommand([]);
-			solargraphCommand(['serialize', vscode.workspace.rootPath]);
-			context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(updateCache));
+		yardCommand([]);
+		solargraphCommand(['serialize', vscode.workspace.rootPath]);
+		context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(updateCache));
+		if (vscode.workspace.getConfiguration("solargraph").useServer) {
+			console.log('Starting the server');
+			solargraphServer = solargraphCommand(['server', '--port', vscode.workspace.getConfiguration("solargraph").serverPort]);
+			solargraphServer.stderr.on('data', (data) => {
+				var out = data.toString();
+				console.log(out);
+				var match = out.match(/port=([0-9]*)/);
+				console.log(match);
+				if (match) {
+					solargraphPort = match[1];
+				}
+			});
 		}
 	});
 	solargraphTest.on('error', () => {
